9d57e78bfcec89a026722b1238983eba5cfecfa7
Andy Kluger <AndyKluger@gmail.com>
2024-12-19 12:58:20 -0500

    Make ctnr build script more readable, add usage, add image name arg

--- mk/ctnr.sh
+++ mk/ctnr.sh
@@ -1,35 +1,61 @@
 #!/bin/sh -e
 cd "$(dirname "$0")"/..
 
+# -- Usage --
+if [ "$1" = -h ] || [ "$1" = --help ]; then
+  printf '%s\n' \
+  'Build a container image for Mouse Ran Down' \
+  "Usage: $0 [<image>]" \
+  '<image> defaults to quay.io/andykluger/mouse-ran-down'
+  exit
+fi
+
+# -- Variables --
 ctnr="$(buildah from python:3.13-alpine)"
 appdir=/app
-image=quay.io/andykluger/mouse-ran-down
+image=${1:-quay.io/andykluger/mouse-ran-down}
 
-buildah run "$ctnr" apk upgrade
-buildah run "$ctnr" apk add ffmpeg s6
+# -- Functions --
+RUN () { buildah run --network host "$ctnr" "$@"; }
+APPEND () { RUN sh -c "cat >>$1"; }
+COPY () { buildah copy "$ctnr" "$@"; }
 
-buildah run "$ctnr" mkdir -p "${appdir}"
+# -- Distro Packages --
+RUN apk upgrade
+RUN apk add ffmpeg s6
+
+# -- Copy App --
+RUN mkdir -p "${appdir}"
 for src in main.py requirements.txt; do
-  buildah copy "$ctnr" "$src" "${appdir}/${src}"
+  COPY "$src" "${appdir}/${src}"
 done
 
-buildah run "$ctnr" python -m venv "${appdir}/.venv"
-buildah run "$ctnr" "${appdir}/.venv/bin/pip" install -r "${appdir}/requirements.txt"
+# -- Python Packages --
+RUN python -m venv "${appdir}/.venv"
+RUN "${appdir}/.venv/bin/pip" install -r "${appdir}/requirements.txt"
 
-buildah run "$ctnr" mkdir -p "${appdir}/logs/app" "${appdir}/svcs/app/log"
-printf '%s\n' \
-  '#!/bin/execlineb -P' 'fdmove -c 2 1' "${appdir}/.venv/bin/python ${appdir}/main.py" \
-  | buildah run "$ctnr" sh -c "cat >${appdir}/svcs/app/run"
-printf '%s\n' \
-  '#!/bin/execlineb -P' s6-log T s4194304 S41943040 "${appdir}/logs/app" \
-  | buildah run "$ctnr" sh -c "cat >${appdir}/svcs/app/log/run"
-buildah run "$ctnr" chmod +x "${appdir}/svcs/app/run" "${appdir}/svcs/app/log/run"
+# -- App Services --
+RUN mkdir -p "${appdir}/svcs/app/log" "${appdir}/logs/app"
 
+<<EOF APPEND "${appdir}/svcs/app/run"
+#!/bin/execlineb -P
+fdmove -c 2 1
+${appdir}/.venv/bin/python ${appdir}/main.py
+EOF
+
+<<EOF APPEND "${appdir}/svcs/app/log/run"
+#!/bin/execlineb -P
+s6-log T s4194304 S41943040 ${appdir}/logs/app
+EOF
+
+RUN chmod +x "${appdir}/svcs/app/run" "${appdir}/svcs/app/log/run"
 buildah config --cmd "s6-svscan ${appdir}/svcs" "$ctnr"
 
+# -- Commit Image --
 imageid="$(buildah commit --rm "$ctnr" "$image")"
 buildah tag "$imageid" "$image:$(date +%Y.%m.%d-%s)"
 
+# -- Tips --
 printf '%s\n' \
   "-- When running container, mount or copy credentials.py into ${appdir}/ --" \
   '-- For example: --' \
