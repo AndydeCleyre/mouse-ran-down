d66a760b44f538fd2b82d7ca31617ab94eaa2623
Andy Kluger <AndyKluger@gmail.com>
2025-03-08 12:57:00 -0500

    Debug: log entities

--- app/main.py
+++ app/main.py
@@ -456,7 +456,9 @@ def bot_mentioned(message: Message) -> bool:
     target = f"@{bot.get_me().username}"
     if message.entities:
         for ent in message.entities:
+            logger.debug("Entity found", entity=ent, type=ent.type)
             if ent.type == 'mention' and get_entity_text(cast(str, message.text), ent) == target:
+                logger.info("Mentioned")
                 return True
     return False
 
